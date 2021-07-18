5f2a63fbf94482a4570fc66ea047dfe517199517
osemudiamen Elebesunu <eronscode@gmail.com>
2021-07-18 18:12:47 +0100

    Added authentication with protected routes

--- api/auth.js
+++ api/auth.js
@@ -1,18 +1,33 @@
 const express = require('express')
 const router = express.Router()
 
-
-const UserModel = require('../models/UserModel')
-const ProfileModel = require('../models/ProfileModel')
-const FollowerModel = require('../models/FollowerModel')
-
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs')
 const isEmail = require('validator/lib/isEmail')
 
+const UserModel = require('../models/UserModel')
+const ProfileModel = require('../models/ProfileModel')
+const FollowerModel = require('../models/FollowerModel')
+const authMiddleware = require("../middleware/authMiddleware");
+
+
 const regexUserName = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/;
 
 
+router.get("/", authMiddleware, async (req, res) => {
+  const { userId } = req;
+
+  try {
+    const user = await UserModel.findById(userId);
+
+    const userFollowStats = await FollowerModel.findOne({ user: userId });
+
+    return res.status(200).json({ user, userFollowStats });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(`Server error`);
+  }
+});
 
 
 router.post("/", async(req, res)=>{
--- utils/hooks/useAuthUser.js
+++ utils/hooks/useAuthUser.js
@@ -1,108 +1,117 @@
-import React, {  useEffect, useState } from 'react';
-import axios from 'axios';
-import baseUrl from '../baseUrl'
-import  catchErrors from '../catchErrors';
-import Router from "next/router"
-import cookie from "js-cookie"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import baseUrl from "../baseUrl";
+import catchErrors from "../catchErrors";
+import Router from "next/router";
+import cookie from "js-cookie";
 
 let cancel;
 
-function useAuthUser(){
-    const [errorMsg, setErrorMsg] = useState(null);
-    const [formLoading, setFormLoading] = useState(false);
+function useAuthUser() {
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [formLoading, setFormLoading] = useState(false);
 
-    async function registerUser(api, user, profilePicUrl){
-        setFormLoading(true)
-        setErrorMsg(null)
-        try {
-            const response =   await axios.post(`${baseUrl}/api/${api}`,{
-                user,
-                profilePicUrl
-            })
+  async function registerUser(api, user, profilePicUrl) {
+    setFormLoading(true);
+    setErrorMsg(null);
+    try {
+      const response = await axios.post(`${baseUrl}/api/${api}`, {
+        user,
+        profilePicUrl,
+      });
 
-            setToken(response.data)
-        } catch (error) {
-            setErrorMsg(catchErrors(error))
-        }
-        setFormLoading(false)
+      setToken(response.data);
+    } catch (error) {
+      setErrorMsg(catchErrors(error));
     }
+    setFormLoading(false);
+  }
 
-    async function loginUser(api, user){
-        setFormLoading(true)
-        setErrorMsg(null)
-        try {
-            const response =   await axios.post(`${baseUrl}/api/${api}`,{
-                user
-            })
+  async function loginUser(api, user) {
+    setFormLoading(true);
+    setErrorMsg(null);
+    try {
+      const response = await axios.post(`${baseUrl}/api/${api}`, {
+        user,
+      });
 
-            setToken(response.data)
-        } catch (error) {
-            setErrorMsg(catchErrors(error))
-        }
-        setFormLoading(false)
+      setToken(response.data);
+    } catch (error) {
+      setErrorMsg(catchErrors(error));
     }
+    setFormLoading(false);
+  }
 
-    return{
-        errorMsg,
-        setErrorMsg,
-        formLoading,
-        setFormLoading,
-        registerUser,
-        loginUser
-    }
-
+  return {
+    errorMsg,
+    setErrorMsg,
+    formLoading,
+    setFormLoading,
+    registerUser,
+    loginUser,
+  };
 }
 
+function useCheckUsername(errorMsg, setErrorMsg, setUser) {
+  const [username, setUsername] = useState("");
+  const [usernameLoading, setUsernameLoading] = useState(false);
+  const [usernameAvailable, setUsernameAvailable] = useState(null);
 
-function useCheckUsername(errorMsg, setErrorMsg, setUser){
-    const [username, setUsername] = useState("")
-    const [usernameLoading, setUsernameLoading] = useState(false)
-    const [usernameAvailable, setUsernameAvailable] = useState(null)
+  async function checkUsername() {
+    setUsernameLoading(true);
+    try {
+      cancel && cancel();
+      const CancelToken = axios.CancelToken;
 
+      const res = await axios.get(`${baseUrl}/api/signup/${username}`, {
+        cancelToken: new CancelToken((canceler) => {
+          cancel = canceler;
+        }),
+      });
 
-    async function checkUsername(){
-        setUsernameLoading(true);
-        try {
-
-          cancel && cancel();
-          const CancelToken = axios.CancelToken;
-
-            const res = await axios.get(`${baseUrl}/api/signup/${username}`,{
-              cancelToken: new CancelToken(canceler => {
-                cancel = canceler;
-              })
-            })
-
-            if(errorMsg!==null) setErrorMsg(null)
-            if(res.data === "Available") setUsernameAvailable(true); setUser(prev => ({...prev, username }));
-        } catch (error) {
-            setErrorMsg("Username Not Available");
-            setUsernameAvailable(false);
-        }
-        setUsernameLoading(false);
+      if (errorMsg !== null) setErrorMsg(null);
+      if (res.data === "Available") setUsernameAvailable(true);
+      setUser((prev) => ({ ...prev, username }));
+    } catch (error) {
+      setErrorMsg("Username Not Available");
+      setUsernameAvailable(false);
     }
+    setUsernameLoading(false);
+  }
 
-    useEffect(()=>{
-        username === "" ? setUsernameAvailable(false):checkUsername()
-    },[username])
+  useEffect(() => {
+    username === "" ? setUsernameAvailable(false) : checkUsername();
+  }, [username]);
 
-    return{
-        username,
-        usernameAvailable,
-        usernameLoading,
-        setUsernameLoading,
-        setUsernameAvailable,
-        setUsername
-    }
+  return {
+    username,
+    usernameAvailable,
+    usernameLoading,
+    setUsernameLoading,
+    setUsernameAvailable,
+    setUsername,
+  };
 }
 
-
-function setToken(token){
-    cookie.set('token', token);
-    Router.push("/")
+function setToken(token) {
+  cookie.set("token", token);
+  Router.push("/");
 }
 
-export {
-    useAuthUser,
-    useCheckUsername
-}
+const redirectUser = (ctx, location) => {
+  if (ctx.req) {
+    ctx.res.writeHead(302, { Location: location });
+    ctx.res.end();
+  } else {
+    Router.push(location);
+  }
+};
+
+const logoutUser = (email) => {
+  cookie.set("userEmail", email);
+  cookie.remove("token");
+  Router.push("/login");
+  Router.reload();
+};
+
+export { useAuthUser, useCheckUsername, redirectUser, logoutUser };
