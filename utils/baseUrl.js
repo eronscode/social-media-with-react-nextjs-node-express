3ef1ad4c2098b3e941dd011fe5a3f4bee2f331b6
eronscode <eronscode@gmail.com>
2021-07-19 17:46:54 +0100

    Deployment

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env === "production"? "https://thrift-social-media.herokuapp.com" : "http://localhost:3000";
+const baseUrl = process.env.NODE_ENV === "production" ? "https://thrift-social-media.herokuapp.com" : "http://localhost:3000";
 
 export default baseUrl;
