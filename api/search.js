6ce4ae85b7feb303004c89f83f4a2ca5b30be44a
eronscode <eronscode@gmail.com>
2021-07-19 13:03:09 +0100

    removed config env file

--- api/search.js
+++ api/search.js
@@ -6,6 +6,7 @@ const UserModel = require("../models/UserModel");
 router.get("/:searchText", authMiddleware, async (req, res) => {
   try {
     const { searchText } = req.params;
+    const { userId } = req;
 
     if (searchText.length === 0) return;
 
@@ -15,7 +16,10 @@ router.get("/:searchText", authMiddleware, async (req, res) => {
       name: { $regex: searchText, $options: "i" }
     });
 
-    return res.status(200).json(results);
+    const resultsToBeSent =
+      results.length > 0 && results.filter(result => result._id.toString() !== userId);
+
+    return res.status(200).json(resultsToBeSent.length > 0 ? resultsToBeSent : results);
   } catch (error) {
     console.error(error);
     return res.status(500).send(`Server error`);
