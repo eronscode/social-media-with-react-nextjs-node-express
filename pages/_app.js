22b5a73ed846c2d4bde86edef8a943295600bd9a
eronscode <eronscode@gmail.com>
2021-07-19 16:52:33 +0100

    Added search page for mobile view

--- pages/_app.js
+++ pages/_app.js
@@ -25,7 +25,8 @@ App.getInitialProps = async ({ Component, ctx }) => {
     ctx.pathname === "/[username]" ||
     ctx.pathname === "/notifications" ||
     ctx.pathname === "/post/[postId]" ||
-    ctx.pathname === "/messages";;
+    ctx.pathname === "/messages" ||
+    ctx.pathname === "/search";
 
   if (!token) {
     protectedRoutes && redirectUser(ctx, "/login");
